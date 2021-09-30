90e9c994649fbbf361e6254ea0c3783f937ed4d5
Umberto Ravaioli <ujrav576@gmail.com>
2021-09-30 08:19:20 -0700

    utility functions moved to separate module

--- solid_py/layout_to_case.py
+++ solid_py/layout_to_case.py
@@ -3,31 +3,63 @@ import sys
 import re
 import json
 import pykle_serial.pykle_serial as kle_serial
-import solid
-from solid import union, difference, translate, rotate, cube
+import math
+from solid import *
+from solid.utils import *
+
+
+U = 19.05
+KEYCAP_LEN = 18
 
 parser = ArgumentParser()
 parser.add_argument('layout', type=str)
 parser.add_argument('output', type=str)
 args = parser.parse_args()
 
-print("loading json file:{}".format(args.layout))
-jsonFile  = open(args.layout,"r") if args.layout else sys.stdin
-layout = jsonFile.read()
-keyboard = kle_serial.parse(layout)
+def main():
+	print("loading json file:{}".format(args.layout))
+	jsonFile  = open(args.layout,"r") if args.layout else sys.stdin
+	layout = jsonFile.read()
+	keyboard = kle_serial.parse(layout)
 
-# import pdb; pdb.set_trace()
+	key_models = union()
+	keycap_models = union()
+	key_footprints = union()
 
-key_models = union()
+	rotated_key = [key for key in keyboard.keys if key.rotation_angle != 0]
 
-for key in keyboard.keys:
-	 key_box = translate([key.rotation_x, key.rotation_y, 0])( rotate([0, 0, key.rotation_angle])( translate([-key.rotation_x, -key.rotation_y, 0])(
-	 	translate([key.x, key.y, 0])(cube([0.9*key.width, 0.9*key.height, 0.1]))
- 	)))
+	for key in keyboard.keys:
+		 key_box = key_model(key)
+		 keycap = keycap_model(key)
+		 footprint = key_plate_footprint(key)
 
-	 key_models += key_box
+		 key_models += key_box
+		 keycap_models += keycap
+		 key_footprints += footprint
 
-scad_str = solid.scad_render(key_models)
 
-with open(args.output, 'w') as f:
-	f.write(scad_str)
+	keycap_models = color([1, 0, 0])(translate([0, 0, 0.1])(keycap_models))
+	key_footprints = color([0, 0, 0])(translate([0, 0, -10])(key_footprints))
+
+	plate = down(10)(down(5)(cube([200, 150, 1.5])) - key_footprints)
+
+	scad_models = key_models + keycap_models + key_footprints + plate
+
+	scad_str = scad_render(scad_models)
+
+	with open(args.output, 'w') as f:
+		f.write(scad_str)
+
+def redox_tight_square_polygon(keys):
+	keys_filtered = [key for key in keys if key.x < 200]
+
+	keys_square = [key for key in keys_filtered if key.rotation_angle == 0]
+	keys_thumb_cluster = [key for key in keys_filtered if key.rotation_angle == 30]
+
+	keys_square_corners = np.array([])
+	for k in keys_square:
+		corners = np.array(key_corners(k))
+		keys_square_corners.append(keys_square_corners, corners, axis=0)
+
+if __name__ == '__main__':
+	main()
---
+++ solid_py/utils.py
@@ -0,0 +1,76 @@
+def key_plate_footprint(key):
+	w = U*key.width
+	h = U*key.height
+	model = key_plate_footprint_model()
+	model = translate([w/2, h/2, 0])(model)
+	model = key_place(key, model)
+	return model
+
+def key_plate_footprint_model():
+	model = cube([14, 14, 10])
+	model = translate([-7, -7, 0])(model)
+	return model
+
+def key_rotation(key, input, from_origin=True):
+	if from_origin:
+		rx = (key.rotation_x - key.x) * U
+		ry = (key.rotation_y - key.y) * U
+	else:
+		rx = (key.rotation_x) * U
+		ry = (key.rotation_y) * U
+
+	return translate([rx, ry, 0])( rotate([0, 0, key.rotation_angle])( 
+		translate([-rx, -ry, 0])(input)))
+
+def key_place(key, model, from_origin=True):
+	model = key_rotation(key, model, from_origin=from_origin)
+	return translate([U*key.x, U*key.y, 0])(model)
+
+def key_model(key):
+	w = U*key.width
+	h = U*key.height
+	model = cube([w, h, 0.1])
+	return key_place(key, model)
+	
+
+def keycap_model(key):
+	keycap_margin = U - KEYCAP_LEN
+	keycap_offset = keycap_margin / 2
+
+	w = U*key.width - keycap_margin
+	h = U*key.height - keycap_margin
+	model = cube([w, h, 0.1])
+	model = translate([keycap_offset, keycap_offset, 0 ])(model)
+
+	model =  key_place(key, model)
+	return model
+
+def key_corners(key):
+	bottom_left = (key.x, key.y)
+	bottom_right = (key.x + key.width, key.y)
+	top_right = (key.x + key.width, key.y + key.height)
+	top_left = (key.x, key.y + key.height)
+
+	bottom_left = key_rotation_apply(key, *bottom_left)
+	bottom_right = key_rotation_apply(key, *bottom_right)
+	top_right = key_rotation_apply(key, *top_right)
+	top_left = key_rotation_apply(key, *top_left)
+
+	return bottom_left, bottom_right, top_right, top_left
+
+def key_rotation_apply(key, x_in, y_in):
+	rot_rad = key.rotation_angle * math.pi/180
+
+	offset_x = key.rotation_x
+	offset_y = key.rotation_y
+
+	offset_x = x_in - key.rotation_x
+	offset_y = y_in - key.rotation_y
+
+	x = offset_x * math.cos(rot_rad) - offset_y * math.sin(rot_rad)
+	y = offset_x * math.sin(rot_rad) + offset_y * math.cos(rot_rad)
+
+	x = x + key.rotation_x
+	y = y + key.rotation_y
+
+	return x,y
